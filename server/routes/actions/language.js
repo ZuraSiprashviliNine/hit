9ccafc4408e9e26889bd149e5265f241ffc27627
ZuraSiprashviliNine <zura.siprashvili.nine@gmail.com>
2018-11-25 22:06:20 +0400

    Reborn

--- server/routes/actions/language.js
+++
@@ -1,9 +0,0 @@
-
-exports.postLanguage = (options = {}) => {
-    return (req, res, next) => {
-        res.cookie('language', req.body.lang);
-        res.locals.language = req.body.lang;
-        res.redirect(req.header('Referer'));
-        // next();
-    }
-}
